67ea77c36d3326bd18439f0559e5768a72ebcaaa
Martin Bruno <mbruno@desarrollosocial.gob.ar>
2024-09-10 22:24:12 -0300

    cambio series controller

--- controllers/series.controller.js
+++ controllers/series.controller.js
@@ -32,7 +32,7 @@ const deleteById = (req, res) => {
     const id = req.params.id
     const idx = series.findIndex(serie => serie.id==id)
     series.splice(idx, 1)
-    res.status(204)
+    res.status(204).json()
 }
 controller.deleteById = deleteById
 
